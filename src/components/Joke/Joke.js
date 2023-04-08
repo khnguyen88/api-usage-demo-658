e7c2a9d0d7049e0d77c6b6bd5e9f04c48c7bd245
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-08 16:54:10 -0400

    Fleshed out the Jokes component.
    Added a state to store the jokes retrieve from the API.
    
    Added a variable which stores a string of the API's URL address.
    
    Added and called the fetchData method, which makes a GET request to the API's URL or link, and accepts back a JSON response.

--- src/components/Joke/Joke.js
+++ src/components/Joke/Joke.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, {useState} from 'react';
 import axios from 'axios';
 
 
 const Joke = () => {
+
+  // State to store jokes retrieved from the API
+  const [joke, setJokes] = useState([]);
+
+  // Variable which website URL address or link of an API
+  const apiLink = "https://icanhazdadjoke.com/"
+
+  // Functions to fetch data from the api via HTTP get request
+  const fetchData = async () => {
+    const response = await axios.get(`${apiLink}`, { headers: { Accept: "application/json" } });
+    console.log(response.data);
+    console.log(response.data.joke);
+  }
   
-  
+  fetchData();
   
   return (
     <div>
