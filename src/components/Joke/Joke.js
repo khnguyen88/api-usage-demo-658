da441aadddfc2edc963f7985e5475533fdae454d
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-08 18:07:11 -0400

    Updated the Joke component's fetchData method to copy the jokes state array, add the joke retrieved from the API response into it, and then set the jokes state variable to the temporary list of jokes.
    Updated the Joke template to include a button that calls the fetchData function during an onClick event and displays the collection of Jokes.

--- src/components/Joke/Joke.js
+++ src/components/Joke/Joke.js
@@ -5,23 +5,41 @@ import axios from 'axios';
 const Joke = () => {
 
   // State to store jokes retrieved from the API
-  const [joke, setJokes] = useState([]);
+  const [jokes, setJokes] = useState([]);
 
   // Variable which website URL address or link of an API
   const apiLink = "https://icanhazdadjoke.com/"
 
   // Functions to fetch data from the api via HTTP get request
+  // We use async function declaration and the await keyword to enable and ensure the fetchData method will run asynchronously 
   const fetchData = async () => {
     const response = await axios.get(`${apiLink}`, { headers: { Accept: "application/json" } });
-    console.log(response.data);
-    console.log(response.data.joke);
+    
+    // Create a shallow copy of the array of jokes
+    const tempJokesList = [...jokes];
+
+    // Add the joke retrieved from API response and add it to the temporary Jokes list.
+    tempJokesList.push(response.data.joke);
+
+    // Update the jokes state variable and set the updated temporary jokes list as the new jokes state
+    setJokes(tempJokesList);
   }
-  
-  fetchData();
   
   return (
     <div>
       <h1>Jokes:</h1>
+      
+      {/* Fetch data from Jokes API */}
+      <button onClick={fetchData}>Retrieve Jokes</button>
+
+      <div className="jokes-container">
+        {/* Display jokes */}
+        {jokes && jokes.map((j, index) => {
+          return (
+            <div className="joke" key={index}> {j} </div>
+          )
+        })}
+      </div>
     </div>
   );
 }
