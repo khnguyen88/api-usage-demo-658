7051d0db99f76d57f58f894290a6085df70cedfd
Khiem Hoang Nguyen <knguyen@jvrenterprises.com>
2023-04-09 13:10:32 -0400

    Updated the App component template to include a header, footer, and main-content containers and areas.

--- src/App.js
+++ src/App.js
@@ -3,9 +3,24 @@ import Joke from './components/Joke/Joke';
 
 function App() {
   return (
+    <>
     <div className="App">
-      <Joke/>
+        <header className="app-header">
+          <div>
+            Welcome to the Dad Zone!!! 
+          </div>
+          <div>
+           (❍ᴥ❍ʋ)
+          </div>
+      </header>
+      <main className="app-main"> 
+        <Joke />
+      </main>       
+      <footer className="app-footer">
+          &#169; Big Poppa Inc., 2023.
+      </footer>
     </div>
+    </>
   );
 }
 
